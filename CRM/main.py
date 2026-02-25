d7e42e99e4de8cfd9573691e20874774416028ba
adarshsingh912786-a11y <adarshsingh912786@gmail.com>
2026-02-25 23:41:08 +0530

    Integreated crm main interface eith client and interaction opertion

---
+++ CRM/main.py
@@ -0,0 +1,83 @@
+from database import (
+    creat_table
+)
+
+from service import (
+    
+    update_client_status,
+    create_client,
+    add_interaction
+)
+
+def main():
+
+    
+     creat_table()
+
+     while True:
+
+        print("....... CRM SYSTEM.......")
+        print("""
+    1. Add client Information
+    2. Add Interaction
+    3. Update Status
+    4. View Record
+    5. Exit
+            
+    """)
+        
+        choice = input("choose from given option: ")
+
+        if choice == "1":
+            name = input("Enter the name of client: ")
+            phone = input("client phone no: ")
+            email = input("client email: ")
+
+            try :
+                create_client(name,phone,email)
+                print("Client add succesfully")
+
+            except ValueError as e:
+                print(f"Error: {e}")
+            except Exception as e:
+                print(f"Error : {e}")
+
+        elif choice == "2":
+
+            client_id = int(input("Enter the client id: "))
+            type = input("Enter the way of Interaction: ")
+            note = input("Leave some note: ")
+
+            try:
+                add_interaction(client_id,type,note)
+            except ValueError as e:
+                print(f"Error : {e}")
+            except Exception as e:
+                print("Error : {e}")        
+
+        elif choice == "3":
+            client_id = int(input("Enter the client id: "))
+            new_status = input("Enter the Updated Staus: ")
+
+            try:
+                update_client_status(client_id,new_status)
+            except ValueError as e:
+                print(f"Error : {e}")
+            except Exception as e:
+                print(f"Error : {e}")    
+
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            print("Goodbye")
+            break
+        else:
+            print("Chooce from givem option")
+
+        
+        
+        
+         
+            
+
+
